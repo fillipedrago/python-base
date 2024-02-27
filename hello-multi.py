aaeafd5ffbea98cd70772f46359676a9cbfa47ee
Fillipe Drago dos Santos <fillipedrago@gmail.com>
2024-02-27 00:42:32 +0000

    Adicionado espanhol e francês.

--- hello-multi.py
+++ hello-multi.py
@@ -31,5 +31,9 @@ if current_language == "pt_BR":
     msg = "Olá, mundo!"
 elif current_language == "it_IT":
     msg: "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg: "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
 
 print(msg)
