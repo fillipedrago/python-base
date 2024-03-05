e3a715df6f4c66e8646ea178c8e8ce63b43c97af
Fillipe Drago dos Santos <fillipedrago@gmail.com>
2024-03-05 21:04:33 +0000

    Script de tabuada corrigido de acordo com aprendizado de formatação de texto.

--- tabuada.py
+++ tabuada.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+   
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
 ...
--------------
-Tabuada do 2
-2
-4
+#################
+---Tabuada do 2---
+   
+    2 x 1 = 2
+    2 x 2 = 4
 ...
 ------------
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Fillipe"
+
 
 # numeros= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros = list(range(1,11))
@@ -22,9 +25,11 @@ numeros = list(range(1,11))
 # Iterable (percorriveis)
 
 # para cada numero em numeros:
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-        # print(numero.__mul__(outro_numero))
-    print("----------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}\n"))
+    print("#" * 18)
+    print()
