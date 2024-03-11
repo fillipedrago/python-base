c6e5ee7414faad26b8bad0ed8953960c8642323d
Fillipe Drago dos Santos <fillipedrago@gmail.com>
2024-03-11 21:48:37 +0000

    Diminuindo a complexidade do script que imprime Hello World

---
+++ escola_v3_com_dict.py
@@ -0,0 +1,41 @@
+#!/home/gitpod/.pyenv/shims/python
+""" Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.1.1"
+
+# Exercício feito com dicionários.
+
+# Dados
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+# Lista com 3 tuplas
+atividades = [
+    ("Inglês", aula_ingles), 
+    ("Música", aula_musica), 
+    ("Dança", aula_danca),
+    ]
+
+# Listar alunos em cada atividade por sala
+
+# É possível desempacotar a tupla dentro de um for:
+
+for nome_atividade, atividade in atividades:
+    print(f"Alunos da atividade {nome_atividade}\n")
+    
+    # sala 1 que tem intersecção com a atividade
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2) & set(atividade)
+
+    
+    print("Sala 1", atividade_sala1)
+    print("Sala 2", atividade_sala2)
+    print("-" * 50)
+
--- hello-multi.py
+++ hello-multi.py
@@ -17,7 +17,7 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Fillipe Drago"
 __license__ = "Unlicensed"
 
@@ -25,15 +25,29 @@ import os
 
 # a segunda entrada da variável getenv é o valor retornado seja nulo             
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
 
-if current_language == "pt_BR":
-    msg = "Olá, mundo!"
-elif current_language == "it_IT":
-    msg: "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg: "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde"
 
-print(msg)
+# sets (Hash Table) - O(1) - constante
+# dicts (Hash Table) - O(1) - constante
+
+msg = {
+    "en_US":"Hello World!",
+    "pt_BR": "Olá, mundo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour Monde",
+    "it_IT": "Ciao, Mondo!",
+}
+
+# Ordem Complexidade O(n):
+
+# if current_language == "pt_BR":
+#     msg = "Olá, mundo!"
+# elif current_language == "it_IT":
+#     msg: "Ciao, Mondo!"
+# elif current_language == "es_SP":
+#     msg: "Hola, Mundo!"
+# elif current_language == "fr_FR":
+#     msg = "Bonjour Monde"
+
+# O(1) - constante
+print(msg[current_language]) #dicionario[chave]
