0bb3d79f01628f0cd81c752a3d22b68021e42a6c
Fillipe Drago dos Santos <fillipedrago@gmail.com>
2024-03-20 21:06:27 +0000

    Arquivo falando sobre possibilidades de como tratar erros

--- errors.py
+++ errors.py
@@ -6,19 +6,38 @@ import sys
 
 
 # LBYL - Look Before You Leap
-if os.path.exists("name.txt"):
-    print("O arquivo existe")
-    input("...") # Race Condition)
-    names = open("names.txt").readlines()
-else:
-    print("[Error] File names.txt not found")
-    sys.exit(1)
 
-if len(names) >= 3:
+# if os.path.exists("name.txt"):
+#     print("O arquivo existe")
+#     input("...") # Race Condition)
+#     names = open("names.txt").readlines()
+# else:
+#     print("[Error] File names.txt not found")
+#     sys.exit(1)
+
+# if len(names) >= 3:
+#     print(names[2])
+# else:
+#     print("[Error] Missing name in the list")
+#     sys.exit(1)
+
+# print(names[2])
+
+
+# EAFP - Easier to Ask Forgiveness than Permission
+try:
+     names = open("names.txt").readlines()
+except FileNotFoundError as e: 
+    print(f"{str(e)}.")
+    sys.exit(1)
+    # TODO: Usar retry
+else: 
+    print ("Sucesso!!!")
+finally:
+    print("Execute isso sempre!")
+
+try:
     print(names[2])
 else:
     print("[Error] Missing name in the list")
     sys.exit(1)
-
-print(names[2])
-
